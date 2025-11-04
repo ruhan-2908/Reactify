9b9c87628e2f5a4b00cb9d20663d7e2e882e32d3
Nishanth10 <nishanthkb10@gmail.com>
2025-11-04 22:55:58 +0530

    Extend Car.js

--- src/components/Car.js
+++ src/components/Car.js
@@ -3,12 +3,17 @@ import { useState } from "react";
 class Car extends React.Component{
     constructor()
     {
-        
+        super();
+        this.state = { color : "Red"}; 
     }
+
     render()
     {
         return (
-            <h2>My Car Color is Red</h2>
+            <>
+            <h2>My Car Color is {this.state.color} </h2>
+            <button onClick={() => { this.setState({color : 'Blue'})}}> Change color</button>
+            </>
         )
     }
 }

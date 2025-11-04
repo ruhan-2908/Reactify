9ac4bc98ee3c4660d8c3ef9dee936eaa441a30ce
Nishanth10 <nishanthkb10@gmail.com>
2025-11-04 22:51:52 +0530

    Add Car.js Component

---
+++ src/components/Car.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { useState } from "react";
+class Car extends React.Component{
+    constructor()
+    {
+        
+    }
+    render()
+    {
+        return (
+            <h2>My Car Color is Red</h2>
+        )
+    }
+}

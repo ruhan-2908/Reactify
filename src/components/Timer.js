50a50c470da2749fdcab0e0a75f64f83779a9d88
ruhan-2908 <ruhankb29@gmail.com>
2025-11-07 22:09:12 +0530

    ..

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -2,21 +2,22 @@ import { useEffect, useState } from "react";
 
 function Timer()
 {
-    const [count, setCount] = useState(1000000);
+    const [count, setCount] = useState(1);
     useEffect(() =>{
         console.log("Screen Rendered");
         // checkCount();
         // setCount(1);
 
-        setTimeout(() =>{
-            setCount((prev) => {return prev-1;})
-        },1)
+        // setTimeout(() =>{
+        //     setCount((prev) => {return prev-1;})
+        // },1000)
     })
 
 
 
     function updateCount()
     {
+        checkCount();
         setCount((prev) => {
             return prev+1;
         })
